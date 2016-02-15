125c65524a84eb8a25a37098396af24b76fb496f
Thanad <thanad@gmail.com>
2016-02-15 15:42:39 +0700

    Update bootstrap.sh

--- .provision/bootstrap.sh
+++ .provision/bootstrap.sh
@@ -3,4 +3,4 @@
 echo -e 'LANG=en_US.UTF-8\nLC_ALL=en_US.UTF-8' > /etc/default/locale
 sleep 1
 apt-get update
-apt-get -y install debootstrap qemu-user-static parted dosfstools
+apt-get -y install build-essential debootstrap dosfstools git libncurses-dev parted qemu-user-static
